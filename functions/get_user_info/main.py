c53fb428dc32b4648f629431881cec4c875a0170
Ruvaakaan <liambrennan0411@gmail.com>
2024-02-22 21:12:06 -0700

    get user info

--- functions/get_user_info/main.py
+++ functions/get_user_info/main.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import jwt
 from botocore.exceptions import ClientError
 
 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/example_dynamodb_Scenario_PartiQLSingle_section.html
@@ -7,17 +8,39 @@ dynamodb_resource = boto3.client("dynamodb")
 
 def get_user_info(event, context):
     try:
-        body = json.loads(event["body"])
-        user_id = body["user_id"]
+        print("event:", event)
+        print("applebanana")
+        
+        # Decode the JWT token from the event
+        token = event.get("token")
+        if not token:
+            return {
+                "statusCode": 400,
+                "body": "JWT token is missing in the request"
+            }
+        
+        decoded_token = jwt.decode(token, algorithms=["RS256"], verify=False)  # Decode the token
+        
+        print("Decoded token:", decoded_token)
+        
+        # Extract user ID from the decoded token
+        user_id = decoded_token.get("sub")
+        if not user_id:
+            return {
+                "statusCode": 400,
+                "body": "User ID is missing in the JWT token"
+            }
 
-        # make sure you use \"<table-name>\" or the query statement won"t work" 
+        # Construct the PartiQL query statement
         statement = "SELECT * FROM \"doodal-users\" WHERE user_id = ?"
         params = [{"S": str(user_id)}]
+
+        # Execute the PartiQL query
         response = dynamodb_resource.execute_statement(
             Statement=statement,
             Parameters=params
         )
-        print(response)
+        print("response:", response)
         
         items = response.get("Items", [])
         if not items:
@@ -32,6 +55,16 @@ def get_user_info(event, context):
             "body": json.dumps(user_info)
         }
         
+    except jwt.ExpiredSignatureError:
+        return {
+            "statusCode": 401,
+            "body": "JWT token has expired"
+        }
+    except jwt.InvalidTokenError:
+        return {
+            "statusCode": 401,
+            "body": "Invalid JWT token"
+        }
     except KeyError:
         return {
             "statusCode": 400,
