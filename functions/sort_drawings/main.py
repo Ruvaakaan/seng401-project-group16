3dd7235eb689d84c52adcee89c19ca17c9450182
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-02-23 00:15:45 -0700

    created sort drawings lambda function

---
+++ functions/sort_drawings/main.py
@@ -0,0 +1,11 @@
+# import boto3
+# import json
+
+# s3 = boto3.client("s3")
+
+def sort_drawings_handler(event, context):
+    print("test")
+    return {
+      "statusCode": 200,
+      "body": "test"
+    }
