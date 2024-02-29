8f74e1ab07b5879f9b7008d804b6e7e08c2dc10e
M-Rehan0 <muhammadrehan.chaudh@ucalgary.ca>
2024-02-29 15:41:19 -0700

    popup updated

--- doodal/src/Popup.js
+++ doodal/src/Popup.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
+import './Popup.css';
 
-const Popup = ({ show, handleClose, selectedImage }) => {
+const Popup = ({ show, handleClose, selectedImage, username }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={show} onHide={handleClose} className="modal-lg">
       <Modal.Header closeButton>
-        <Modal.Title>Image Popup</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {selectedImage && (
-          <img src={selectedImage} alt="Popup" style={{ width: "100%" }} />
-        )}
+        <div style={{ textAlign: "center" }}>
+          {selectedImage && (
+            <img
+              // src={selectedImage}
+              src="octopus.PNG" // temp image src. set it to {selectedImage} 
+              alt="Popup image"
+              className="popup-image"
+            />
+          )}
+        </div>
       </Modal.Body>
+      <Modal.Footer className="popup-footer">
+          <div className="user_info">
+            <img className="logo" src="octopus.PNG" alt="Logo" />
+            {/* <span className="name">{username}</span> */}
+            <span className="name">Username</span> 
+          </div>
+          <div className="like-btn" onClick={handleLike} >
+            {liked ? (
+                    <button className="like">
+                      &#9829;
+                    </button>
+                  ) : (
+                    <button className="like">
+                      &#9825;
+                    </button>
+                  )}
+          </div>
+      </Modal.Footer>
     </Modal>
   );
 };
