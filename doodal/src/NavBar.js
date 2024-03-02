a50cb5d9c6a7d3c05dffc1323a99ecd5d9c17fc7
freshmint7 <djosephbuisness@gmail.com>
2024-03-02 15:56:07 -0700

    draw page removed from navbar

--- doodal/src/NavBar.js
+++ doodal/src/NavBar.js
@@ -21,9 +21,9 @@ function NavBar({ loggedIn }) {
           <Link to="/gallery" className="items">
             Gallery
           </Link>
-          <Link to="/draw" className="items">
+          {/* <Link to="/draw" className="items">
             Draw
-          </Link>
+          </Link> */}
           {loggedIn !== false ? (
             <div className="dropdown-click">
               <div
