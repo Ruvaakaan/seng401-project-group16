4b6387b172c0934ed636a1ee673517f850c9a713
EthanBensler <ethan.bensler@ucalgary.ca>
2024-03-02 15:22:55 -0700

    makeApiCall function get and post working

--- doodal/src/getImages.js
+++ doodal/src/getImages.js
@@ -1,21 +1,14 @@
+import makeApiCall from "./makeApiCall";
+
 export const getImages = async (id) => {
-  let res = await fetch(
-    `https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_competition_drawings`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        competition_id: id,
-      }),
-    }
-  );
-  let extracted = await res.json();
+  const request = JSON.stringify({
+    competition_id: id,
+  })
+  let res = await makeApiCall(`https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_competition_drawings`, "POST", request)
 
-  let { body } = extracted;
+  let body = res.body;
   body = JSON.parse(body);
-
+  
   let image_list = [];
   try {
     for (let i = 0; i < body["items"].length; i++) {
