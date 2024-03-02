eb52ef7b009f1615412527076e9534abe762e73a
freshmint7 <djosephbuisness@gmail.com>
2024-03-02 00:51:40 -0700

    error handling

--- doodal/src/getImages.js
+++ doodal/src/getImages.js
@@ -1,27 +1,29 @@
 export const getImages = async (id) => {
-    let res = await fetch(
-      `https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_drawings`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          competition_id: id,
-        }),
-      }
-    );
-    let extracted = await res.json();
-    
-    let { body } = extracted;
-    body = JSON.parse(body);
-    console.log(body)
-    let image_list = [];
-
-    for (let i = 0; i < body["items"].length; i++) {
-      let url = body["items"][i]["s3_url"]["S"]; 
-      image_list.push(url); 
+  let res = await fetch(
+    `https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_drawings`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        competition_id: id,
+      }),
     }
-    console.log(image_list)
+  );
+  let extracted = await res.json();
+
+  let { body } = extracted;
+  body = JSON.parse(body);
+
+  let image_list = [];
+  try {
+    for (let i = 0; i < body["items"].length; i++) {
+      let url = body["items"][i]["s3_url"]["S"];
+      image_list.push(url);
+    }
     return image_list;
-  };
+  } catch {
+    return [];
+  }
+};
