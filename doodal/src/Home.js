df36254729780ac41594bf42ff0086d711c6f246
freshmint7 <djosephbuisness@gmail.com>
2024-03-01 21:38:45 -0700

    get images on homepage

--- doodal/src/Home.js
+++ doodal/src/Home.js
@@ -6,13 +6,13 @@ import { useEffect, useState } from "react";
 import { useDarkMode } from "./DarkModeContext";
 
 function Home() {
-  var prompt = "new";
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const nav = useNavigate();
 
   const [current, setCurrent] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const [comps, setComps] = useState([]);
   const [prompts, setPrompts] = useState([]);
+  const [images, setImages] = useState([]);
 
   const getPrompts = async () => {
     let res = await fetch(
@@ -32,30 +32,44 @@ function Home() {
     for (let i = 0; i < 3; i++) {
       // take 3 most recent prompts
       newComps.push(body[i]["competition_id"]["S"]);
+      await getImages(i, body[i]["competition_id"]["S"]);
       newPrompts.push(body[i]["prompt"]["S"]);
     }
     setComps(newComps);
     setPrompts(newPrompts);
-    // getImages();
   };
 
-  const getImages = async () => {
-    // 2 requests to get each set? (previous prompt images and current prompt images)
+  const getImages = async (index, id) => {
+    let res = await fetch(
+      `https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_drawings`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          competition_id: id,
+        }),
+      }
+    );
+    let extracted = await res.json();
+    let { body } = extracted;
+    let image_list = body['image_urls'];
 
-    // let res = await fetch(``, {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-    // let extracted = await res.json();
-    // setCurrent(extracted);
-    console.log("get imgs");
+    if (!image_list){
+      setImages(images => [...images, []]);
+      return;
+    }
+    setImages(images => [...images, image_list]);
   };
 
   useEffect(() => {
     getPrompts();
   }, []);
+
+  // useEffect(()=> {
+  //   console.log(images);
+  // }, [images])
 
   return (
     <>
@@ -72,12 +86,16 @@ function Home() {
           <>
             <div className="prompt-display">
               <div className="prompt-button">
-                <h1 className="prompt-title">Current Competition: {prompts[index]}</h1>
+                <h1 className="prompt-title">
+                  Ongoing Competition: {prompts[index]}
+                </h1>
                 <Button
                   variant="outline-dark"
                   className="entry-button"
                   onClick={() =>
-                    nav("/gallery", { state: { additionalProp: prompts[index] } })
+                    nav("/gallery", {
+                      state: { prompt: prompts[index], comp_id: comps[index] },
+                    })
                   }
                 >
                   View
@@ -91,11 +109,15 @@ function Home() {
               pagination={{ clickable: true }}
               navigation
             >
-              {current.map((item, index) => (
-                <SwiperSlide key={index}>
-                  <img src="octopus.PNG" width={550} className="home-imgs" />
-                </SwiperSlide> // temp image, item should hold the image so use {item}
-              ))}
+              {images[index]?.length === 0 ? (
+                <h1>No images yet!</h1>
+              ) : (
+                images[index].map((item, index) => (
+                  <SwiperSlide key={index}>
+                    <img src={item} width={550} className="home-imgs" />
+                  </SwiperSlide>
+                ))
+              )}
             </Swiper>
           </>
         ))
