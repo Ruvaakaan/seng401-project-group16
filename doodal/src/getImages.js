3bfc4311d3ac69525fb9f9fc85aead2a17b047f8
freshmint7 <djosephbuisness@gmail.com>
2024-03-01 22:15:17 -0700

    easier reuse of getimages

---
+++ doodal/src/getImages.js
@@ -0,0 +1,19 @@
+export const getImages = async (id) => {
+    let res = await fetch(
+      `https://p7kiqce3wh.execute-api.us-west-2.amazonaws.com/test/get_drawings`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          competition_id: id,
+        }),
+      }
+    );
+    let extracted = await res.json();
+    let { body } = extracted;
+    let image_list = body['image_urls'];
+
+    return image_list;
+  };
