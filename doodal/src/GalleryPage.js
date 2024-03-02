1c76e271e7d8a7a042f7eb0b7c62a324055ff85b
freshmint7 <djosephbuisness@gmail.com>
2024-03-02 00:20:04 -0700

    sort drawings stub + getimages for gallery

--- doodal/src/GalleryPage.js
+++ doodal/src/GalleryPage.js
@@ -3,8 +3,9 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Button } from "react-bootstrap";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import { getImages } from "./getImages.js";
+import { sortImages } from "./sortDrawings.js";
 
 function GalleryPage() {
   const [user_likes, setUserLikes] = useState([1, 3]); // array of all posts liked by user
@@ -16,16 +17,15 @@ function GalleryPage() {
   const prompt = location.state?.prompt;
   const comp_id = location.state?.comp_id;
 
-  var title = "Gallery"
+  var title = "Gallery";
 
-  useEffect(()=> {
-    if (prompt){ 
-        title = prompt;
-        setUserEnter(true);
-      }
-      handleImages(comp_id);
-    
-  }, [])
+  useEffect(() => {
+    if (prompt) {
+      title = prompt;
+      setUserEnter(true);
+    }
+    handleImages(comp_id);
+  }, []);
 
   function like_change(val) {
     if (user_likes.includes(val)) {
@@ -49,78 +49,84 @@ function GalleryPage() {
         }),
       }
     );
-    if (res.ok){
-      console.log("success")
+    if (res.ok) {
+      console.log("success");
     }
-  }
+  };
 
   const handleImages = async (id) => {
-    
     let image_list = await getImages(id);
 
-    if (!image_list){
-      setImages(images => [...images, []]); // change how this is handled here
+    if (!image_list) {
       return;
     }
-    setImages(images => [...images, image_list]);
+    setImages(...images, image_list);
   };
 
   const callSorter = async (s) => {
-    // const link = "hello/"+s;
-    // console.log(link);
-    // const res = await fetch(
-    //   ``,
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // );
-    // const extracted = await res.json();
+    
+    let sorted = await sortImages(s);
 
-    let v = Math.floor(Math.random() * 10);
-    let extracted = new Array(v).fill(1);
-    setImages(extracted);
+    // let v = Math.floor(Math.random() * 10);
+    // let extracted = new Array(v).fill(1);
+    // setImages(extracted);
   };
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
 
   return (
     <>
-    <div className="gallery-banner">
-      <h1 className="gallery-title">{title}</h1>
-      {userEnter && (
-      <Button
-                  variant="outline-dark"
-                  className="entry-button"
-                  onClick={() =>
-                    nav("/draw", {
-                      state: { prompt: prompt, comp_id: comp_id },
-                    })
-                  }
-                ></Button>
-
-      )}
-      <div className="filter-options">
-        <ul className="filter-list">
-          <li className="filter">
-            <b>Filter:</b>
-          </li>
-          <li className="filter-item reg-hover" onClick={()=>callSorter("hot")}>Hot</li>
-          <li className="filter-item reg-hover"onClick={()=>callSorter("pop")}>Popular</li>
-          <li className="filter-item reg-hover"onClick={()=>callSorter("new")}>Newest</li>
-        </ul>
+      <div className="gallery-banner">
+        <h1 className="gallery-title">{title}</h1>
+        {userEnter && (
+          <Button
+            variant="outline-dark"
+            className="entry-button"
+            onClick={() =>
+              nav("/draw", {
+                state: { prompt: prompt, comp_id: comp_id },
+              })
+            }
+          ></Button>
+        )}
+        <div className="filter-options">
+          <ul className="filter-list">
+            <li className="filter">
+              <b>Filter:</b>
+            </li>
+            <li
+              className="filter-item reg-hover"
+              onClick={() => callSorter("hot")} // change these values, add the other options from the lambda
+            >
+              Hot
+            </li>
+            <li
+              className="filter-item reg-hover"
+              onClick={() => callSorter("pop")}
+            >
+              Popular
+            </li>
+            <li
+              className="filter-item reg-hover"
+              onClick={() => callSorter("new")}
+            >
+              Newest
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
       <div className="gal">
         <Row xs={6} className="g-4">
           {images.map((val, idx) => (
             <Col key={idx}>
               <Card>
-                <Card.Img variant="top" src="doodalnew.PNG" />
+                <Card.Img variant="top" src={val} />
                 <Card.Body id="card">
                   <div className="user_info">
                     <img src="octopus.PNG" width={60} />
-                    <text className="name">Username{val}</text>
+                    <text className="name">Username{idx}</text>
                   </div>
                   {user_likes.includes(val) ? (
                     <button className="like" onClick={() => like_change(val)}>
