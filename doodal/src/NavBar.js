563301f244f5737d6a3d6f86478a7df05a6f0b35
Ruvaakaan <liambrennan0411@gmail.com>
2024-03-16 23:57:34 -0600

    extra

--- doodal/src/NavBar.js
+++ doodal/src/NavBar.js
@@ -21,72 +21,12 @@ function NavBar({ loggedIn }) {
     : null;
 
   const handleLogOut = () => {
-    if (Cookies.get("authentication")) {
-      Cookies.remove("authentication");
-      Cookies.remove("userInfo");
-      window.location.reload();
-    }
+    Cookies.remove("authentication");
+    Cookies.remove("userInfo");
+    window.location.reload();
+  
   };
-
-  // return (
-  //   <>
-  //     <div id="bar">
-  //       <div className="logos">
-  //         <img
-  //           src="https://doodals-bucket-seng401.s3.us-west-2.amazonaws.com/website+photos/octopus.PNG"
-  //           alt="logo"
-  //           width={150}
-  //         ></img>
-  //         <Link to="/home">
-  //           <img
-  //             src="https://doodals-bucket-seng401.s3.us-west-2.amazonaws.com/website+photos/doodalnew.png"
-  //             alt="doodal"
-  //             width={275}
-  //             id="doodal"
-  //           ></img>
-  //         </Link>
-  //       </div>
-  //       <div className="nav">
-  //         <Link to="/home" className="items">
-  //           Home
-  //         </Link>
-  //         <Link to="/gallery" className="items">
-  //           Gallery
-  //         </Link>
-  //         {loggedIn !== false ? (
-  //           <div className="dropdown-click">
-  //             <div
-  //               className={
-  //                 isDropdownOpen
-  //                   ? "profile dropdown-opened reg-hover"
-  //                   : "profile reg-hover"
-  //               }
-  //               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-  //             >
-  //               <Image
-  //                 src={
-  //                   profilePhotoUrl
-  //                     ? profilePhotoUrl
-  //                     : "https://doodals-bucket-seng401.s3.us-west-2.amazonaws.com/website+photos/octopus.PNG"
-  //                 }
-  //                 roundedCircle
-  //                 className="profile-photo-nav"
-  //               />
-  //             </div>
-  //             {isDropdownOpen && <DropdownMenu />}
-  //           </div>
-  //         ) : (
-  //           <Link
-  //             to="https://doodal.auth.us-west-2.amazoncognito.com/login?client_id=6c1og3jvcp62aqmkhjcgkjkvgq&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F"
-  //             className="items"
-  //           >
-  //             Login
-  //           </Link>
-  //         )}
-  //       </div>
-  //     </div>
-  //   </>
-  // );
+  
 
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
@@ -178,7 +118,7 @@ function NavBar({ loggedIn }) {
                 {/* <Nav.Link> */}
                   <Link
                     to="https://doodal.auth.us-west-2.amazoncognito.com/login?client_id=6c1og3jvcp62aqmkhjcgkjkvgq&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F"
-                    className="nav-link-custom"
+                    className="nav-link-custom-extra"
                   >
                     Login
                   </Link>
