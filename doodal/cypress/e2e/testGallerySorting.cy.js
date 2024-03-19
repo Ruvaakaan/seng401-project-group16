ee20b12930ec60afe92b7ca49f5c9c773487d644
EthanBensler <ethan.bensler@ucalgary.ca>
2024-03-18 18:58:31 -0600

    small fixes to gallery sorting

--- doodal/cypress/e2e/testGallerySorting.cy.js
+++ doodal/cypress/e2e/testGallerySorting.cy.js
@@ -1,111 +1,132 @@
 describe("Gallery Sorting", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
 
-    cy.url().should("eq", "http://localhost:3000/");
-
-    cy.get(".swiper-wrapper .swiper-slide-active") // Target the first  tile
-      .within(() => {
-        cy.get(".card-img-flush").click(); // Click the image within the first tile
-      });
-  });
-
-  it("Sorts by Least Liked", () => {
-    cy.get('.filter-list li.filter-item:contains("Least Liked")').click();
-    //delay needed while values sorted
-    cy.wait(1000)
-    // Get all like counters in the order displayed
-    cy.get('.gal .row .col .card .like-container .like-counter').then(($likeCounters) => {
-      cy.log('$likeCounters');
-      cy.log('test')
-
-      for (let i = 1; i < $likeCounters.length; i++) {
-        const previousCount = parseInt($likeCounters.eq(i - 1).text());
-        const currentCount = parseInt($likeCounters.eq(i).text());
-        cy.wrap(currentCount).should('be.gte', previousCount);
-      }
+        cy.url().should("eq", "http://localhost:3000/");
+        cy.wait(2000)
+          cy.get(".swiper-wrapper .swiper-slide-active") // Target the first  tile
+            .within(() => {
+              cy.get(".card-img-flush").click(); // Click the image within the first tile
+            });
     });
-  });
 
+    it("Sorts by Least Liked", () => {
+        cy.get('.filter-list li.filter-item:contains("Least Liked")').click();
+        //delay needed while values sorted
+        cy.wait(1000);
+        // Get all like counters in the order displayed
+        cy.get(".gal .row .col .card .like-container .like-counter").then(
+            ($likeCounters) => {
+                cy.log("$likeCounters");
+                cy.log("test");
 
-  it("Sorts by Most Liked", () => {
-    cy.get('.filter-list li.filter-item:contains("Most Liked")').click();
-    //delay needed while values sorted
-    cy.wait(1000)
-    // Get all like counters in the order displayed
-    cy.get('.gal .row .col .card .like-container .like-counter').then(($likeCounters) => {
-      for (let i = 1; i < $likeCounters.length; i++) {
-        const previousCount = parseInt($likeCounters.eq(i - 1).text());
-        const currentCount = parseInt($likeCounters.eq(i).text());
-        cy.wrap(currentCount).should('be.lte', previousCount);
-      }
+                for (let i = 1; i < $likeCounters.length; i++) {
+                    const previousCount = parseInt(
+                        $likeCounters.eq(i - 1).text()
+                    );
+                    const currentCount = parseInt($likeCounters.eq(i).text());
+                    cy.wrap(currentCount).should("be.gte", previousCount);
+                }
+            }
+        );
     });
-  });
 
-  function convertToSeconds(timeString) {
-    const timeParts = timeString.split(" ");
-    const value = parseInt(timeParts[1]);
-    const unit = timeParts[2].toLowerCase();
+    it("Sorts by Most Liked", () => {
+        cy.get('.filter-list li.filter-item:contains("Most Liked")').click();
+        //delay needed while values sorted
+        cy.wait(1000);
+        // Get all like counters in the order displayed
+        cy.get(".gal .row .col .card .like-container .like-counter").then(
+            ($likeCounters) => {
+                for (let i = 1; i < $likeCounters.length; i++) {
+                    const previousCount = parseInt(
+                        $likeCounters.eq(i - 1).text()
+                    );
+                    const currentCount = parseInt($likeCounters.eq(i).text());
+                    cy.wrap(currentCount).should("be.lte", previousCount);
+                }
+            }
+        );
+    });
 
-    switch (unit) {
-      case "seconds":
-        return value;
-      case "minutes":
-        return value * 60;
-      case "hours":
-        return value * 60 * 60;
-      case "days":
-        return value * 60 * 60 * 24;
-      default:
-        throw new Error(`Unsupported time unit: ${unit}`);
+    function convertToSeconds(timeString) {
+        const timeParts = timeString.split(" ");
+        const value = parseInt(timeParts[1]);
+        const unit = timeParts[2].toLowerCase();
+
+        switch (unit) {
+            case "second":
+                return value;
+            case "seconds":
+                return value;
+            case "minute":
+                return value * 60;
+            case "minutes":
+                return value * 60;
+            case "hour":
+                return value * 60 * 60;
+            case "hours":
+                return value * 60 * 60;
+            case "days":
+                return value * 60 * 60 * 24;
+            case "day":
+                return value * 60 * 60 * 24;
+            default:
+                throw new Error(`Unsupported time unit: ${unit}`);
+        }
     }
-  }
 
-  it("Sorts by Newest", () => {
+    it("Sorts by Newest", () => {
+        const convertedTimes = [];
 
-    const convertedTimes = [];
+        cy.get('.filter-list li.filter-item:contains("Newest")').click();
+        cy.wait(1000);
+        cy.get(".gal .row .col .card").each(($card, index) => {
+            // Click each card to open the popup
+            cy.wrap($card).click();
 
-    cy.get('.filter-list li.filter-item:contains("Newest")').click();
+            // Extract posted time from the popup
+            cy.get(".posted-time")
+                .invoke("text")
+                .then(($postedTime) => {
+                    const convertedTime = convertToSeconds($postedTime.trim());
 
-    cy.get('.gal .row .col .card').each(($card, index) => {
-      // Click each card to open the popup
-      cy.wrap($card).click();
+                    convertedTimes.push(convertedTime);
+                    if (index > 0) {
+                        cy.wrap(convertedTimes[index]).should(
+                            "be.gte",
+                            convertedTimes[index - 1]
+                        );
+                    }
+                });
+            cy.get(".btn-close").click();
+        });
+    });
 
-      // Extract posted time from the popup
-      cy.get('.posted-time').invoke('text').then(($postedTime) => {
-        const convertedTime = convertToSeconds($postedTime.trim());
+    it("Sorts by Oldest", () => {
+        const convertedTimes = [];
 
-        convertedTimes.push(convertedTime);
-        if (index > 0) {
-          cy.wrap(convertedTimes[index]).should('be.gte', convertedTimes[index - 1]);
-        }
-      });
-      cy.get('.btn-close').click();
-    })
-  });
+        cy.get('.filter-list li.filter-item:contains("Oldest")').click();
+        cy.wait(1000);
+        cy.get(".gal .row .col .card").each(($card, index) => {
+            // Click each card to open the popup
+            cy.wrap($card).click();
 
-  it("Sorts by Oldest", () => {
+            // Extract posted time from the popup
+            cy.get(".posted-time")
+                .invoke("text")
+                .then(($postedTime) => {
+                    const convertedTime = convertToSeconds($postedTime.trim());
 
-    const convertedTimes = [];
-
-    cy.get('.filter-list li.filter-item:contains("Oldest")').click();
-
-    cy.get('.gal .row .col .card').each(($card, index) => {
-      // Click each card to open the popup
-      cy.wrap($card).click();
-
-      // Extract posted time from the popup
-      cy.get('.posted-time').invoke('text').then(($postedTime) => {
-        const convertedTime = convertToSeconds($postedTime.trim());
-
-        convertedTimes.push(convertedTime);
-        if (index > 0) {
-          cy.wrap(convertedTimes[index]).should('be.lte', convertedTimes[index - 1]);
-        }
-      });
-      cy.get('.btn-close').click();
-    })
-  });
-
-
+                    convertedTimes.push(convertedTime);
+                    if (index > 0) {
+                        cy.wrap(convertedTimes[index]).should(
+                            "be.lte",
+                            convertedTimes[index - 1]
+                        );
+                    }
+                });
+            cy.get(".btn-close").click();
+        });
+    });
 });
